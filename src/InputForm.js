dc78a6ffdd2f3470b7a1005c8e3cdcdc9e26de22
Delian Zidarov <d.zidarov@gmail.com>
2021-12-02 16:34:04 +0900

    did the password match interface

--- src/InputForm.js
+++ src/InputForm.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./InputForm.css";
 
-function InputForm({ name, type, handleChange, label }) {
-  console.log(name, type, handleChange, label);
+function InputForm({ name, type, handleChange, label, alert }) {
   return (
     <div>
       <h3>{label}</h3>
       <input
+        className={alert ? (alert === "true" ? `red` : `green`) : null}
         name={name}
         type={type}
         onChange={handleChange}
--- src/SignUp.js
+++ src/SignUp.js
@@ -2,10 +2,22 @@ import React from "react";
 import InputForm from "./InputForm";
 
 function SignUp() {
-  const handleChange = () => {};
-  const name = "firstName";
-  const type = "text";
-  const label = "First Name";
+  const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    userName: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [formData, setFormData] = React.useState(initialState);
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
   return (
     <div className="form-container">
       <h1>Hello</h1>
@@ -45,8 +57,13 @@ function SignUp() {
           label="Confirm Password"
           handleChange={handleChange}
           type="password"
+          alert={
+            formData.password !== formData.confirmPassword ? "true" : "false"
+          }
         />
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
       </form>
     </div>
   );
